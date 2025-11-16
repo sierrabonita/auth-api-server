9640d636f76944eea0a892c1a1674d5f59b70984
sierrabonita <workbj1212@gmail.com>
2025-11-16 09:01:53 +0900

    feat: Add cookie-parser middleware for handling cookies in the application

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import cookieParser from "cookie-parser";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // @Req() req から req.cookies.refresh_tokenを読めるようにする
+  app.use(cookieParser());
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
